7eaad9a22b143b42fe5bddbe689af237fe2ef732
Igor Palyvoda <ihor.palyvoda@inoxoft.com>
2023-08-26 13:57:27 +0300

    Initial Commit

---
+++ TestMediaDropBox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestMediaDropBox
+//
+//  Created by Igor Palyvoda on 26.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
